abd03c5e027da0f1ff90bc9e5493ad434ca5edba
alanwtf <alan.mathiasen@outlook.com>
2020-05-23 16:50:47 -0300

    added some bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+
+
 let plus = (a, b) => a + b;
 
 let minus = (a, b) => a - b;
@@ -12,11 +14,17 @@ let currentOperation = '';
 
 let nextOperation = '';
 
+let operationLast = false;
+
 const numbers = document.querySelectorAll('.number');
 const operations = document.querySelectorAll('.operation');
 const input = document.querySelector('#input');
 const equals = document.querySelector('#equals');
 const clear = document.querySelector('#clear');
+
+document.onload = clearDisplay();
+
+document.addEventListener('keypress', keyboardKeyPressed);
 
 numbers.forEach(number => number.addEventListener('click', numberButtonPressed));
 
@@ -24,14 +32,42 @@ operations.forEach(operation => operation.addEventListener(('click'), handleOper
 
 equals.addEventListener('click', handleOperation);
 
-clear.addEventListener('click', function(){
-    input.value = '';
-    arrayNumbers = [];
-    console.table(arrayNumbers);
-} );
+clear.addEventListener('click', clearDisplay);
 
-function numberButtonPressed(){
-    input.value += this.value;
+function keyboardKeyPressed(e){
+    let numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let numbersIds = ['zero', 'one', 'two', 'three', 'four','five', 'six', 'seven',
+        'eight','nine'];
+    let action = (e.keyCode === 13) ? 'enter' : String.fromCharCode(e.keyCode);
+    
+    if(!isNaN(action) && action in numbersArray)
+        document.getElementById(numbersIds[action]).click();
+    else {  
+        switch(action){
+            case '+':
+                document.getElementById('plus-btn').click();
+                break;
+            case '-':
+                document.getElementById('minus-btn').click();
+                break;
+            case '*':
+                document.getElementById('multiply-btn').click();
+                break;
+            case '/':
+                document.getElementById('divide-btn').click();
+                break;
+            case 'enter':
+                document.getElementById('equals').click();
+        }
+    }
+}
+
+function numberButtonPressed(e, value = null){
+    if(operationLast) 
+        input.value= '';
+    operationLast = false;
+    input.value += (value != null)? value : this.value;
+
 }
 
 function displayNumber(num){
@@ -40,22 +76,35 @@ function displayNumber(num){
 
 
 function handleOperation(){
-    if(numbersArray.length === 0){
+    if(numbersArray.length === 0 && !operationLast){
         numbersArray.push(input.value);
         currentOperation = this.value;
-    } else if (numbersArray.length >= 1 && currentOperation !== 'equals'){
+    } else if (numbersArray.length >= 1 && currentOperation !== 'equals' && !operationLast){
         console.log(this.value);
         numbersArray.push(input.value);
         nextOperation = this.value;
         let num1 = parseFloat(numbersArray[numbersArray.length - 2]);
         let num2 = parseFloat(numbersArray[numbersArray.length - 1]);
         let result = operationes(currentOperation, num1, num2);
+        if(isNaN(result)){
+            clearDisplay();
+            return;
+        }
         numbersArray.push(result);
         displayNumber(result);
         currentOperation = nextOperation;
+        operationLast = true;
     } else {
         currentOperation = this.value;
+        
     }
+    operationLast = true;
+}
+
+function clearDisplay(){
+    input.value = '';
+    arrayNumbers = [];
+    console.table(arrayNumbers);
 }
 
 function operationes(result, num1, num2){
@@ -67,6 +116,8 @@ function operationes(result, num1, num2){
         case 'multiply':
             return multiply(num1, num2);
         case 'divide':
+            if(num2 === 0)
+                return alert('You broke the universe');
             return divide(num1, num2);
     }
 }
